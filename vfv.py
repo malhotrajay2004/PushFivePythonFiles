c41bb4c7d6c3ba3c3660ca6f7b874ae1d349ffed
malhotrajay2004 <93135690+malhotrajay2004@users.noreply.github.com>
2023-07-03 09:33:42 +0530

    This Is First Python File

---
+++ vfv.py
@@ -0,0 +1,8 @@
+myname = "jay"
+brothername = "deep"
+sistername="vaishnavi"
+age = 18
+result =76.6
+print(f"my name is {myname} my brothername is {brothername}  my sistername is {sistername} my age is {age} my result is {result}")
+print(f"the addition of {age} and {result} is : {age+result}")
+print(f"the substraction of {age} and {result} is : {age-result}")
